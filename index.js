7fb602ccfa288c8ae75c4672ce724111e1ca2f43
ashen <1989712521>
2021-09-09 15:54:49 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-#!/usr/bin/env/ node
-
 const program = require('commander');
-var process1 = require('child_process');
+const process1 = require('child_process');
 //version 版本号
 //name 新项目名称
 program.version('1.0.0', '-v, --version')
