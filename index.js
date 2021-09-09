c8008223eee3f0d563adc7aa0339f6044cf59c74
ashen <1989712521>
2021-09-09 15:50:14 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env/ node
+
+const program = require('commander');
+var process1 = require('child_process');
+//version 版本号
+//name 新项目名称
+program.version('1.0.0', '-v, --version')
+    .command('init <name>')
+    .action((name) => {
+        console.log('clone template ...');
+        process1.exec('git clone http://10.106.11.64/liushengzhi/my-dd-mini-program-cli-template.git ' + name, function(error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+                return;
+            }
+            console.log(stdout);
+            console.log('clone success');
+        });
+    });
+program.parse(process.argv);
