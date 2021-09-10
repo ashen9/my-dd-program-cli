b47fea6382f07121f5dc576fad69e196c5f2c971
ashen <1989712521>
2021-09-10 11:07:21 +0800

    latest version

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 // 首先会到env设置里查找node的安装路径，再调用对应路径下的解释器程序完成操作。
 const program = require('commander');
 const process1 = require('child_process');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const symbols = require('log-symbols');
+const chalk = require('chalk');
+const ora = require("ora");
+const handlebars = require("handlebars");
+const path = require('path');
 //version 版本号
 //name 新项目名称
 program.version('1.0.0', '-v, --version')
@@ -10,14 +17,56 @@ program.version('1.0.0', '-v, --version')
     .description('初始化项目模版...')
     .action((name) => {
         console.log('clone template ...');
-        process1.exec('git clone http://10.106.11.64/liushengzhi/my-dd-mini-program-cli-template.git ' + name, function(error, stdout, stderr) {
-            if (error !== null) {
-                console.log('exec error: ' + error);
-                return;
+        if(fs.existsSync(name)) {
+            console.log(symbols.error, chalk.red('项目已存在'))
+            return
+        }
+        inquirer.prompt([
+            {
+                name: "description",
+                message: "请输入项目描述"
+            },
+            {
+                name: "author",
+                message: "请输入作者名称"
             }
-            console.log(stdout);
-            console.log('clone success');
-        });
+        ]).then(answers => {
+            process1.exec('git clone http://10.106.11.64/liushengzhi/my-dd-mini-program-cli-template.git ' + name, function(error, stdout, stderr) {
+                /*if (error !== null) {
+                    console.log('exec error: ' + error);
+                    return;
+                }
+                console.log(stdout);
+                console.log('clone success');*/
+                const spinner = ora("正在下载模版...")
+                spinner.start();
+                if (error !== null) {
+                    spinner.fail()
+                    console.log(symbols.error, chalk.red(`拉取远程仓库模版失败${error}`))
+                }
+                spinner.succeed()
+                const meta = {
+                    name,
+                    description: answers.description,
+                    author: answers.author
+                }
+                const fileName = `${name}/package.json`
+
+                // 模板目录
+                // const tmplDir = path.join(__dirname, 'templates') // __dirname:当前模块的目录名
+                // 目标目录
+                const destDir = process.cwd() // Node.js 进程的当前工作目录。
+                // console.log(path.join(destDir, fileName), fs.existsSync(path.join(destDir, fileName)))
+                if (fs.existsSync(path.join(destDir, fileName))) {
+                    const content = fs.readFileSync(fileName).toString()
+                    const result = handlebars.compile(content)(meta)
+                    // console.log(symbols.success, chalk.green(content), chalk.green(result))
+                    fs.writeFileSync(path.join(destDir, fileName), result)
+                }
+                console.log(symbols.success, chalk.green("项目初始化完成"))
+            });
+
+        })
     });
 // 解析命令行
 program.parse(process.argv);
